42e31020c2760083717d61eb8a1eaecaf4f79d46
Bruno Machado <brunomachado3818@gmail.com>
2023-03-10 18:14:53 -0300

    PokeApiService with HttpClient and Observables

--- src/app/shared/poke-list/poke-list.component.ts
+++ src/app/shared/poke-list/poke-list.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { PokeApiService } from 'src/app/service/poke-api.service';
 
 @Component({
   selector: 'poke-list',
   templateUrl: './poke-list.component.html',
   styleUrls: ['./poke-list.component.scss'],
 })
-export class PokeListComponent {}
+export class PokeListComponent {
+  constructor(private pokeApiService: PokeApiService) {
+    pokeApiService.apiListAllPokemons.subscribe((res) => console.log(res));
+  }
+}
